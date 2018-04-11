fd2f13247d4b11a6cc56879f1bae09b5d6c68612
Akash Patra <patraakash9@gmail.com>
2018-04-11 23:34:50 +0530

    Initial Commit

---
+++ src/in/co/trapps/jvsk/java/Hello.java
@@ -0,0 +1,10 @@
+package in.co.trapps.jvsk.java;
+
+/**
+ * @author Akash Patra
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("From Java: Hello Akash :)");
+    }
+}
---
+++ src/in/co/trapps/jvsk/kotlin/Hello.kt
@@ -0,0 +1,8 @@
+package `in`.co.trapps.jvsk.kotlin
+
+/**
+ * @author Akash Patra
+ */
+fun main(args: Array<String>) {
+    println("From Kotlin: Hi Akash :)")
+}
