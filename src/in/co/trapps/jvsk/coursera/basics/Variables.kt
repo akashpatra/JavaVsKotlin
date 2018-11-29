6839d5714ede100744c5d2981c432fcb2c0dedff
Akash Patra <patraakash9@gmail.com>
2018-11-29 14:21:36 +0530

    Kotlin Variables Examples

---
+++ src/in/co/trapps/jvsk/coursera/basics/Variables.kt
@@ -0,0 +1,12 @@
+package `in`.co.trapps.jvsk.coursera.basics
+
+/**
+ * @author Akash Patra
+ */
+fun variableTypeList() {
+    val list = mutableListOf("Java")
+    list.add("Kotlin")
+
+    var nmList = listOf("Java")
+//        nmList.add
+}
