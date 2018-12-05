0a49652855e481b1bb5b7c41fc7fe0f7d501fc44
Akash Patra <patraakash9@gmail.com>
2018-12-05 19:54:50 +0530

    Kotlin: How to handle exceptions from Java Code

---
+++ src/in/co/trapps/jvsk/coursera/control_structures/CS.java
@@ -0,0 +1,21 @@
+package in.co.trapps.jvsk.coursera.control_structures;
+
+import java.io.IOException;
+
+/**
+ * @author Akash Patra
+ */
+public class CS {
+
+    public static void main(String[] args) {
+        exceptionTest();
+    }
+
+    private static void exceptionTest() {
+        try {
+            ExceptionsKt.foo();
+        } catch (IOException e) {
+
+        }
+    }
+}
