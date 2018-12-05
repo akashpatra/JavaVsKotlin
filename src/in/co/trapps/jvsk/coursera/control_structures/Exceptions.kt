4aded1146cdf8551774673d68b572750b637a851
Akash Patra <patraakash9@gmail.com>
2018-12-05 19:52:10 +0530

    Kotlin: Exceptions examples

---
+++ src/in/co/trapps/jvsk/coursera/control_structures/Exceptions.kt
@@ -0,0 +1,11 @@
+package `in`.co.trapps.jvsk.coursera.control_structures
+
+import java.io.IOException
+
+/**
+ * @author Akash Patra
+ */
+@Throws(IOException::class)
+fun foo() {
+    throw IOException()
+}
