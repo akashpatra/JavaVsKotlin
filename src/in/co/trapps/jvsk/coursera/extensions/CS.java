e0dfa1953e2b530c8fa75fc5157c6b85f31d894d
Akash Patra <patraakash9@gmail.com>
2018-12-12 20:21:43 +0530

    Kotlin: Extensions Examples from Kotlin and Java Code

---
+++ src/in/co/trapps/jvsk/coursera/extensions/CS.java
@@ -0,0 +1,15 @@
+package in.co.trapps.jvsk.coursera.extensions;
+
+/**
+ * @author Akash Patra
+ */
+public class CS {
+
+    public static void main(String[] args) {
+        extensionFunctionTest();
+    }
+
+    private static void extensionFunctionTest() {
+        System.out.println(ExtFuncKt.lastButOneChar("qwe"));
+    }
+}
